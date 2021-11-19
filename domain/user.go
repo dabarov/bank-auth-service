43d78c2c9a91fdc06fb2e562d39987f710c4e587
dabarov <simlyalde@gmail.com>
2021-11-19 14:47:17 +0500

    feat: add user struct

--- domain/user.go
+++ domain/user.go
@@ -1,1 +1,11 @@
 package domain
+
+type User struct {
+	ID        int64  `json:"id"`
+	IIN       int64  `json:"iin"`
+	Name      string `json:"name"`
+	Surname   string `json:"surname"`
+	Phone     string `json:"phone"`
+	Role      string `json:"role"`
+	CreatedAt string `json:"created_at"`
+}
