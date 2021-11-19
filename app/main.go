5c86e99e10d6adc765cec19f7ff6d6ac2f79f44d
dabarov <simlyalde@gmail.com>
2021-11-19 15:23:52 +0500

    feat: starter for sign in handler

--- app/main.go
+++ app/main.go
@@ -1,4 +1,64 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/buaazp/fasthttprouter"
+	"github.com/dabarov/online-banking/domain"
+	"github.com/valyala/fasthttp"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	username = "postgres"
+	password = "postgres"
+	hostname = "localhost"
+	port     = 5432
+	db       = "postgres"
+)
+
+type MyRESTServer struct {
+	db *gorm.DB
+}
+
+func extractCredential(ctx *fasthttp.RequestCtx) (login string, pass string) {
+	return string(ctx.FormValue("login")), string(ctx.FormValue("password"))
+}
+
+func (s *MyRESTServer) SignUp(ctx *fasthttp.RequestCtx) {
+}
+
+func (s *MyRESTServer) SignIn(ctx *fasthttp.RequestCtx) {
+	login, password := extractCredential(ctx)
+
+	var user domain.User
+	s.db.Where(&domain.User{Login: login, Password: password}).First(&user)
+
+	if user.Name == "" {
+		log.Println("Invalid credentials!")
+		ctx.SetStatusCode(fasthttp.StatusForbidden)
+		fmt.Fprintf(ctx, "Invalid credentials!")
+		return
+	}
+}
+
 func main() {
+	DSN := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", username, password, hostname, port, db)
+	db, err := gorm.Open(postgres.Open(DSN), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	server := &MyRESTServer{
+		db: db,
+	}
+	db.AutoMigrate(&domain.User{})
+
+	router := fasthttprouter.New()
+	router.POST("/signup", server.SignUp)
+	router.POST("/signin", server.SignIn)
+	fasthttp.ListenAndServe(":8080", router.Handler)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,25 @@ module github.com/dabarov/online-banking
 
 go 1.17
 
-require ()
+require (
+	github.com/buaazp/fasthttprouter v0.1.1
+	github.com/valyala/fasthttp v1.31.0
+	gorm.io/gorm v1.22.3
+	github.com/andybalholm/brotli v1.0.2 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.10.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.1.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.8.1 // indirect
+	github.com/jackc/pgx/v4 v4.13.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gorm.io/driver/postgres v1.2.2 // indirect
+)
