b6dfedcdedc104d644c3c19129ff309a0b92852e
Aldiyar Dabarov <simlyalde@gmail.com>
2021-12-05 21:34:17 +0600

    feat: add interfaces in user domain

--- domain/user.go
+++ domain/user.go
@@ -1,12 +1,22 @@
 package domain
 
+import "context"
+
 type User struct {
 	IIN       uint64 `json:"iin"`
 	Login     string `json:"login"`
 	Password  string `json:"password"`
-	Name      string `json:"name"`
-	Surname   string `json:"surname"`
-	Phone     string `json:"phone"`
-	Role      string `json:"role"`
 	CreatedAt string `json:"created_at"`
+}
+
+type UserUsecase interface {
+	GetByIIN(ctx context.Context, IIN uint64) (User, error)
+	SignUp(ctx context.Context, user *User) error
+	SignIn(ctx context.Context, user *User) error
+}
+
+type UserRepository interface {
+	GetByIIN(ctx context.Context, IIN uint64) (User, error)
+	SignUp(ctx context.Context, user *User) error
+	SignIn(ctx context.Context, user *User) error
 }
