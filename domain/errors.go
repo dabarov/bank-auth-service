fe01502cec8a6f76fb5aec2c9e4b0f07414c3c99
Aldiyar Dabarov <simlyalde@gmail.com>
2021-12-08 02:32:01 +0600

    feat: add error messages

--- domain/errors.go
+++ domain/errors.go
@@ -1,1 +1,10 @@
 package domain
+
+import "errors"
+
+var (
+	ErrLoginTaken  = errors.New("user with such username already exists")
+	ErrEmptyField  = errors.New("login or password field is empty")
+	ErrIINTaken    = errors.New("user with such IIN already exists")
+	ErrIINIncorect = errors.New("incorect fomat of IIN")
+)
